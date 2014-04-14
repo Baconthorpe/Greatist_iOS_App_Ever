9518035f38cdc37c89761f26870037c2eff38770
Baconthorpe <zabuhoff@gmail.com>
2014-04-14 11:02:57 -0400

    it builds but doesn't display article cells

---
+++ Greatist Message Publisher/Greatist Message Publisher/Model/Article+Methods.h
@@ -0,0 +1,13 @@
+//
+//  Article+Methods.h
+//  Greatist Message Publisher
+//
+//  Created by Ezekiel Abuhoff on 4/14/14.
+//  Copyright (c) 2014 Ezekiel Abuhoff. All rights reserved.
+//
+
+#import "Article.h"
+
+@interface Article (Methods)
+
+@end
---
+++ Greatist Message Publisher/Greatist Message Publisher/Model/Article+Methods.m
@@ -0,0 +1,13 @@
+//
+//  Article+Methods.m
+//  Greatist Message Publisher
+//
+//  Created by Ezekiel Abuhoff on 4/14/14.
+//  Copyright (c) 2014 Ezekiel Abuhoff. All rights reserved.
+//
+
+#import "Article+Methods.h"
+
+@implementation Article (Methods)
+
+@end
