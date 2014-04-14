a616b33956d6f91a033c6b1a6c28082e6abd643c
Baconthorpe <zabuhoff@gmail.com>
2014-04-14 12:28:00 -0400

    added method for discerning a unique user object

--- Greatist Message Publisher/Greatist Message Publisher/Model/GRTImporter.m
+++ Greatist Message Publisher/Greatist Message Publisher/Model/GRTImporter.m
@@ -7,7 +7,30 @@
 //
 
 #import "GRTImporter.h"
+#import "Response+Methods.h"
+#import "Post+Methods.h"
 
 @implementation GRTImporter
+
+//+ (Response *) saveResponseFromDictionary: (NSDictionary *)responseDictionary
+//                          toContext: (NSManagedObjectContext *)context
+//{
+//    return nil;
+//}
+//
+//+ (Post *) savePostFromDictionary: (NSDictionary *)postDictionary
+//                        toContext: (NSManagedObjectContext *)context
+//{
+//    NSMutableSet *responseSet = [NSMutableSet new];
+//    
+//    for (NSDictionary *response in postDictionary[@"responses"])
+//    {
+//        Response *newResponse = [Response responseWithResponseOption:<#(ResponseOption *)#> post:nil author:<#(User *)#> inContext:<#(NSManagedObjectContext *)#>]
+//    }
+//    
+//    Post *newPost = [Post postWithContent:postDictionary[@"content"] author:postDictionary[@"author"] section:postDictionary[@"section"] responses:<#(NSSet *)#> inContext:context];
+//    
+//    return nil;
+//}
 
 @end
