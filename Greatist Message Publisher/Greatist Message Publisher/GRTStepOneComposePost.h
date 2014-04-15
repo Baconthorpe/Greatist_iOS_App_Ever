c3353e942a63e369e3523db30d21e2c4ca4560da
Anne Lindsley <anne.lindsley@flatironschool.com>
2014-04-14 21:11:48 -0400

    updated storyboard and start laying groundwork with xibs for new modal compose post funcationality

---
+++ Greatist Message Publisher/Greatist Message Publisher/GRTStepOneComposePost.h
@@ -0,0 +1,13 @@
+//
+//  GRTStepOneComposePost.h
+//  Greatist Message Publisher
+//
+//  Created by Anne Lindsley on 4/14/14.
+//  Copyright (c) 2014 Ezekiel Abuhoff. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface GRTStepOneComposePost : UIView
+
+@end
---
+++ Greatist Message Publisher/Greatist Message Publisher/GRTStepThreeComposePostViewController.h
@@ -0,0 +1,13 @@
+//
+//  GRTStepThreeComposePostViewController.h
+//  Greatist Message Publisher
+//
+//  Created by Anne Lindsley on 4/14/14.
+//  Copyright (c) 2014 Ezekiel Abuhoff. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface GRTStepThreeComposePostViewController : UIViewController
+
+@end
