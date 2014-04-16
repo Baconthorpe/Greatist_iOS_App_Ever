24a423c4c238c06860ccc22401223a3a87a7a33b
Len Li <leonard.li@gmail.com>
2014-04-16 10:58:51 -0400

    Create Data Manager as singleton class

---
+++ Greatist Message Publisher/Greatist Message Publisher/Model/GRTDataManager.m
@@ -0,0 +1,60 @@
+//
+//  GRTDataManager.m
+//  Greatist Message Publisher
+//
+//  Created by Leonard Li on 4/16/14.
+//  Copyright (c) 2014 Ezekiel Abuhoff. All rights reserved.
+//
+
+#import "GRTDataManager.h"
+
+@implementation GRTDataManager
+
++ (GRTParseAPIClient *)defaultParseClient
+{
+    return [GRTParseAPIClient sharedClient];
+}
+
++ (GRTFacebookAPIClient *)defaultFacebookClient
+{
+    return [GRTFacebookAPIClient sharedClient];
+}
+
++ (NSManagedObjectContext *)defaultManagedObjectContext
+{
+    return [GRTDataStore sharedDataStore].managedObjectContext;
+}
+
++ (instancetype)sharedManager {
+   
+    return [[self alloc] init];
+}
+
+- (instancetype)init
+{
+    return [self initWithParseAPIClient:[[self class] defaultParseClient]
+                      FacebookAPIClient:[[self class] defaultFacebookClient]
+                   ManagedObjectContext:[[self class] defaultManagedObjectContext]];
+}
+
+- (instancetype)initWithParseAPIClient:(GRTParseAPIClient *)parseClient
+                     FacebookAPIClient:(GRTFacebookAPIClient *)facebookClient
+                  ManagedObjectContext:(NSManagedObjectContext *)managedObjectContext
+{
+    
+    static GRTDataManager *_sharedManager = nil;
+    static dispatch_once_t onceToken;
+    dispatch_once(&onceToken, ^{
+        _sharedManager = [[GRTDataManager alloc] init];
+        if (_sharedManager) {
+            _parseAPIClient = parseClient;
+            _facebookAPIClient = facebookClient;
+            _managedObjectContext = managedObjectContext;
+        }
+    });
+
+    return _sharedManager;
+}
+
+
+@end
