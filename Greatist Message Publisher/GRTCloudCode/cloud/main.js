b4ad14a40d52239c827e386d23b4fe7558c808b1
Baconthorpe <zabuhoff@gmail.com>
2014-04-18 14:11:43 -0400

    edited cloud code

--- Greatist Message Publisher/GRTCloudCode/cloud/main.js
+++ Greatist Message Publisher/GRTCloudCode/cloud/main.js
@@ -1,6 +1,50 @@
 
 // Use Parse.Cloud.define to define as many cloud functions as you want.
 // For example:
-Parse.Cloud.define("hello", function(request, response) {
-  response.success("Hello world!");
-});
+ 
+Parse.Cloud.define("GRTUser", function(request, response){
+    var query = new Parse.Query('GRTUser');
+    query.find({
+        success: function(results){
+            response.success(results); 
+        }, 
+        error: function(){
+            response.error("user lookup failed"); 
+        }
+ 
+    }); 
+ 
+}); 
+Parse.Cloud.define("friendPosts", function(request, response){
+    var query = new Parse.Query("GRTUser"); 
+    query.containedIn("facebookID", request.params.facebook_ids_array);
+    query.find({
+        success: function(results){
+             
+            console.log("results =" + results[0]); 
+ 
+            var friendIDs = new Array(); 
+             
+            for (var i = 0; i < results.length; i++)
+            {
+                friendIDs.push(results[i].get("facebookID")); 
+            }
+             
+ 
+            var query = new Parse.Query("GRTPost");
+            query.containedIn("facebookID", friendIDs); 
+            query.find ({
+                success: function(results){
+                    console.log(results);
+                    response.success(results);  
+                }, 
+                error: function(){
+                    response.error("Post lookup failed"); 
+                }
+            }); 
+        },
+        error: function(){
+            response.error("Friend lookup failed"); 
+        }
+    });
+}); 
